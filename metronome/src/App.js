246dd6b60f50020b4e5ced7d6f24c4f0e2763163
GrantvanB <gvanbo@gmail.com>
2021-09-08 16:40:05 -0600

    initial architecture is in place

--- metronome/src/App.js
+++ metronome/src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
+import Main from './pages/main/main.page';
 
 function App() {
   return (
-    <div>
-      <h1>Logo</h1>
-      <h1>Visualize</h1>
-      <h1>Start and Stop</h1>
-      <h1>Tempo / Time Signature</h1>
-      <h1>Tempo Dropdown</h1>
-      <h1>Time Signature DropDown</h1>
-    </div>
+    <>
+      <Main />
+    </>
   );
 }
 
