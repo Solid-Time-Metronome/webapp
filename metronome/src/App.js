f1284e6c1278de24fa75953b5128b638efd990fc
GrantvanB <gvanbo@gmail.com>
2021-09-08 16:11:59 -0600

    updated app.js to h1 archecture guide

--- metronome/src/App.js
+++ metronome/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Logo</h1>
+      <h1>Visualize</h1>
+      <h1>Start and Stop</h1>
+      <h1>Tempo / Time Signature</h1>
+      <h1>Tempo Dropdown</h1>
+      <h1>Time Signature DropDown</h1>
     </div>
   );
 }
