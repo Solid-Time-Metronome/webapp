dca97fff8bf911dfb7135938a6c79890c57ae719
GrantvanB <gvanbo@gmail.com>
2021-09-09 15:50:41 -0600

    added some formatting to vizualize

--- metronome/src/components/visualize/visualize.component.jsx
+++ metronome/src/components/visualize/visualize.component.jsx
@@ -2,8 +2,8 @@ import './visualize.style.css'
 
 export default function Visualize () {
     return(
-        <>
+        <div class='visualizeContainer'>
             <h1>Metronome Visualization</h1>
-        </>
+        </div>
     )
 }
