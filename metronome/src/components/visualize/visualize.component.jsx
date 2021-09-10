919005c226619134c170322034cc66e86e4ffbcc
kyo6919 <78553494+kyo6919@users.noreply.github.com>
2021-09-10 02:05:17 -0600

    Button + Visualized
    Moved Button container into Visualized component due to child parent prop passing requirement.
    
    function logics are defined in visualized component right now
    
    onclick functions are passed down into child button components.
    
    changed stylessheet to use name.module.css as correct way to do module css

--- metronome/src/components/visualize/visualize.component.jsx
+++ metronome/src/components/visualize/visualize.component.jsx
@@ -1,9 +1,57 @@
-import './visualize.style.css'
+import styles from "./visualize.module.css";
+import { useState, useEffect } from "react";
+import ButtonContainer from "../../components/buttonContainer/buttonContainer.component";
+import audio from "../../assets/sounds/highClick.mp3";
 
-export default function Visualize () {
-    return(
-        <div class='visualizeContainer'>
-            <h1>Metronome Visualization</h1>
-        </div>
-    )
-}
+const Visualize = () => {
+	//starting blinking state
+	const [isActive, setIsActive] = useState(false); //timer state
+	const [isBlinking, setIsBlinking] = useState(false); //color change state
+
+	//need to figure out the math for the bpm and the duration of each beat.
+
+	const time = 1000; //current set to 1 sec
+
+	useEffect(() => {
+		let id;
+
+		if (isActive === true) {
+			id = setInterval(() => {
+				//console.log(`${isBlinking}`);
+				setIsBlinking(!isBlinking); //toggle true and false
+				if (isBlinking === true) {
+					new Audio(audio).play();
+				}
+			}, time);
+		}
+
+		return () => clearInterval(id);
+	}, [isActive, isBlinking]);
+
+	//start timer function
+	const onStartClick = () => {
+		console.log("start");
+		setIsActive(true);
+	};
+	//stop timer function
+	const onStopClick = () => {
+		console.log("stop");
+		setIsActive(false);
+	};
+
+	return (
+		<>
+			<div className={styles.visualizeContainer}>
+				<h1 className={isBlinking ? `${styles.green}` : `${styles.red}`}>
+					Metronome Visualization
+				</h1>
+			</div>
+			<ButtonContainer
+				onStartClick={onStartClick}
+				onStopClick={onStopClick}
+			/>
+		</>
+	);
+};
+
+export default Visualize;
--- metronome/src/pages/main/main.page.jsx
+++ metronome/src/pages/main/main.page.jsx
@@ -11,7 +11,7 @@ export default function Main () {
       <div>
         <Logo />
         <Visualize />
-        <ButtonContainer />
+        
         <SelectorsContainer />
       </div>  
     )
