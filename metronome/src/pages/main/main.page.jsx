9f2f7312955d8f44f1837be1a31699f077b8490a
GrantvanB <gvanbo@gmail.com>
2021-09-15 10:24:46 -0600

    fixed alll linting and proptype issues

--- metronome/src/pages/main/main.page.jsx
+++ metronome/src/pages/main/main.page.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import Visualize from "../../components/visualize/visualize.component";
-import Logo from "../../components/logo/logo.component";
+import React from 'react'
+import Visualize from '../../components/visualize/visualize.component'
+import Logo from '../../components/logo/logo.component'
 
-import "./main.style.css";
+import './main.style.css'
 
-export default function Main() {
+export default function Main () {
   return (
     <div>
       <Logo />
       <Visualize />
     </div>
-  );
+  )
 }
