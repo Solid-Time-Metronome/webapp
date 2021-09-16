55b411d89e5d70a30214abb72f39ec8ae58bce28
kyo6919 <78553494+kyo6919@users.noreply.github.com>
2021-09-16 01:08:07 -0600

    eslint issues

--- metronome/src/App.js
+++ metronome/src/App.js
@@ -1,22 +1,22 @@
-import "./App.css";
-import React from "react";
-import Main from "./pages/main/main.page";
-import About from "./pages/about/about.page";
-import Nav from "./components/Nav/Nav.component";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import './App.css'
+import React from 'react'
+import Main from './pages/main/main.page'
+import About from './pages/about/about.page'
+import Nav from './components/Nav/Nav.component'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-function App() {
-	return (
-		<Router>
-			<div className="App">
-				<Nav />
-				<Switch>
-					<Route path="/" exact component={Main} />
-					<About path="/about" component={About} />
-				</Switch>
-			</div>
-		</Router>
-	);
+function App () {
+  return (
+      <Router>
+         <div className='App'>
+            <Nav />
+            <Switch>
+               <Route path='/' exact component={Main} />
+               <About path='/about' component={About} />
+            </Switch>
+         </div>
+      </Router>
+  )
 }
 
-export default App;
+export default App
--- metronome/src/Util/useDocumentTitle.js
+++ metronome/src/Util/useDocumentTitle.js
@@ -1,21 +1,21 @@
 // useDocumentTitle.js
-import { useRef, useEffect } from "react";
+import { useRef, useEffect } from 'react'
 
-function useDocumentTitle(title, prevailOnUnmount = false) {
-	const defaultTitle = useRef(document.title);
+function useDocumentTitle (title, prevailOnUnmount = false) {
+  const defaultTitle = useRef(document.title)
 
-	useEffect(() => {
-		document.title = title;
-	}, [title]);
+  useEffect(() => {
+    document.title = title
+  }, [title])
 
-	useEffect(
-		() => () => {
-			if (!prevailOnUnmount) {
-				document.title = defaultTitle.current;
-			}
-		},
-		[]
-	);
+  useEffect(
+    () => () => {
+      if (!prevailOnUnmount) {
+        document.title = defaultTitle.current
+      }
+    },
+    []
+  )
 }
 
-export default useDocumentTitle;
+export default useDocumentTitle
