31a5336795bcec2fa0a91738a1bc92888e2ea452
Your NameMegan <you@example.comxu.megan@hotmail.com>
2021-09-21 08:08:17 -0600

    fixing css

--- metronome/src/components/showBeats/showBeats.component.jsx
+++ metronome/src/components/showBeats/showBeats.component.jsx
@@ -1,32 +1,32 @@
-import  React  from 'react'
-import styles from './showBeats.module.css';
-import PropTypes from 'prop-types';
-
-export default function ShowBeats({countbeat,beats}) {
-  beats = 6;//should be the input
-  var data1=[]
-  var data2=[...Array(beats).keys()];
-  var cleanBeat = Math.floor(countbeat?countbeat:0);
-  var measureBeat = ((cleanBeat - 1) % beats) + 1;
- for (let i =1; i <= measureBeat;i++){
-   data1.push(i);
-  }   
+import styles from './showBeats.module.css'
+import PropTypes from 'prop-types'
+export default function ShowBeats ({ countbeat, beats }) {
+  beats = 6// should be the input
+  const overlaydata = [...Array(beats).keys()]
+  const cleanBeat = Math.floor(countbeat)
+  const measureBeat = ((cleanBeat - 1) % beats) + 1
+  const topdata = []
+  for (let i = 1; i <= measureBeat; i++) {
+    topdata.push(i)
+  }
   return (
+    <div className={styles.wrapper}>
     <div className={styles.showbeats}>
-    <div className={styles.child1}>
-      {data1.map((item) => (
-        <span className={styles.showbeat1} key={item}></span>
-      ))}    
+      <div className={styles.overlay1}>
+        {topdata.map((item) => (
+          <span className={styles.showbeat1} key={item}></span>
+        ))}
+      </div>
+      <div className={styles.overlay2}>
+        {overlaydata.map((item) => (
+          <span className={styles.showbeat2} key={item}></span>
+        ))}
+      </div>
     </div>
-    <div className={styles.child2}>
-      {data2.map((item) => (
-        <span className={styles.showbeat2} key={item}></span>  
-      ))}
     </div>
-  </div>
-  );
+  )
 }
 ShowBeats.propTypes = {
   beats: PropTypes.number,
-  countbeat: PropTypes.number,
+  countbeat: PropTypes.number
 }
