6d12bf15e2f8e8ea270c63e1b3df6f5d263aa78c
Your NameMegan <you@example.comxu.megan@hotmail.com>
2021-09-20 20:29:06 -0600

    change showbeats to cycles, and sync with beats

--- metronome/src/components/showBeats/showBeats.component.jsx
+++ metronome/src/components/showBeats/showBeats.component.jsx
@@ -1,19 +1,32 @@
-import { React } from 'react'
+import  React  from 'react'
 import styles from './showBeats.module.css';
 import PropTypes from 'prop-types';
 
-export default function ShowBeats({countbeat,beats,BPM}) {
-    beats = 4;//should be the input
-    var cleanBeat = Math.floor(countbeat?countbeat:0);
-    var measureBeat = ((cleanBeat - 1) % beats) + 1;
-    return ( 
-      <div className={styles.showbeat}>  
-       <span className={BPM ? `${styles.big}` : `${styles.small}`}>{measureBeat}</span>
-      </div>   
-    )
-  }
-  ShowBeats.propTypes = {
-    beats: PropTypes.number,
-    countbeat: PropTypes.number,
-    BPM: PropTypes.number
-  }
+export default function ShowBeats({countbeat,beats}) {
+  beats = 6;//should be the input
+  var data1=[]
+  var data2=[...Array(beats).keys()];
+  var cleanBeat = Math.floor(countbeat?countbeat:0);
+  var measureBeat = ((cleanBeat - 1) % beats) + 1;
+ for (let i =1; i <= measureBeat;i++){
+   data1.push(i);
+  }   
+  return (
+    <div className={styles.showbeats}>
+    <div className={styles.child1}>
+      {data1.map((item) => (
+        <span className={styles.showbeat1} key={item}></span>
+      ))}    
+    </div>
+    <div className={styles.child2}>
+      {data2.map((item) => (
+        <span className={styles.showbeat2} key={item}></span>  
+      ))}
+    </div>
+  </div>
+  );
+}
+ShowBeats.propTypes = {
+  beats: PropTypes.number,
+  countbeat: PropTypes.number,
+}
