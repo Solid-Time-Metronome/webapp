aeca4dbd05b7db92fc839fb68ef124f6d27a7167
GrantvanB <gvanbo@gmail.com>
2021-09-22 13:55:36 -0600

    fixed About page spacing

--- metronome/src/components/FoundingTeam/FoundingTeam.component.jsx
+++ metronome/src/components/FoundingTeam/FoundingTeam.component.jsx
@@ -2,9 +2,8 @@ import styles from './FoundingTeam.module.css'
 import megan from '../../assets/graphics/megan.jpg'
 import grant from '../../assets/graphics/grant.jpg'
 import jimmy from '../../assets/graphics/jimmy.jpg'
-import React, { useState } from 'react'
-//import { react, useState } from 'react'
-import propTypes from 'prop-types'
+import { useState } from 'react'
+// import { react, useState } from 'react'
 
 const FoundingTeam = () => {
   const [isClickMegan, setisClickMegan] = useState(false)
@@ -23,62 +22,64 @@ const FoundingTeam = () => {
   }
 
   return (
-      <>
-         <h1>FOUNDING TEAM</h1>
+    <>
+      <div className={styles.founderbody}>
+        <h1>FOUNDING TEAM</h1>
 
-         <div className={styles.flexRow}>
-            <div className={styles.imgContainer} onClick={onClickMegan}>
-               <img
-                  src={megan}
-                  alt=''
-                  className={isClickMegan ? styles.opacity : ''}
-               />
+        <div className={styles.flexRow}>
+          <div className={styles.imgContainer} onClick={onClickMegan}>
+            <img
+              src={megan}
+              alt=""
+              className={isClickMegan ? styles.opacity : ''}
+            />
 
-               {isClickMegan
-                 ? (
-                  <div className={styles.absolute}>
-                     <p className={styles.founderName}>Megan, CTO</p>
-                     <p>Write some stuff</p>
-                  </div>
-                   )
-                 : null}
-            </div>
+            {isClickMegan
+              ? (
+              <div className={styles.absolute}>
+                <p className={styles.founderName}>Megan, CTO</p>
+                <p>Write some stuff</p>
+              </div>
+                )
+              : null}
+          </div>
 
-            <div className={styles.imgContainer} onClick={onClickJImmy}>
-               <img
-                  src={jimmy}
-                  alt=''
-                  className={isClickJimmy ? styles.opacity : ''}
-               />
+          <div className={styles.imgContainer} onClick={onClickJImmy}>
+            <img
+              src={jimmy}
+              alt=""
+              className={isClickJimmy ? styles.opacity : ''}
+            />
 
-               {isClickJimmy
-                 ? (
-                  <div className={styles.absolute}>
-                     <p className={styles.founderName}>Jimmy, CTO</p>
-                     <p>write some stuff later</p>
-                  </div>
-                   )
-                 : null}
-            </div>
+            {isClickJimmy
+              ? (
+              <div className={styles.absolute}>
+                <p className={styles.founderName}>Jimmy, CTO</p>
+                <p>write some stuff later</p>
+              </div>
+                )
+              : null}
+          </div>
 
-            <div className={styles.imgContainer} onClick={onClickGrant}>
-               <img
-                  src={grant}
-                  alt=''
-                  className={isClickGrant ? styles.opacity : ''}
-               />
+          <div className={styles.imgContainer} onClick={onClickGrant}>
+            <img
+              src={grant}
+              alt=""
+              className={isClickGrant ? styles.opacity : ''}
+            />
 
-               {isClickGrant
-                 ? (
-                  <div className={styles.absolute}>
-                     <p className={styles.founderName}>Grant, CEO</p>
-                     <p>write some stuff</p>
-                  </div>
-                   )
-                 : null}
-            </div>
-         </div>
-      </>
+            {isClickGrant
+              ? (
+              <div className={styles.absolute}>
+                <p className={styles.founderName}>Grant, CEO</p>
+                <p>write some stuff</p>
+              </div>
+                )
+              : null}
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
