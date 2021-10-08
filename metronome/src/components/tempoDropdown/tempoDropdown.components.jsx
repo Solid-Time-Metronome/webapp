7b6db5cbca2d1ff59ac92c64f33ebd4c6973b816
GrantvanB <gvanbo@gmail.com>
2021-10-08 14:55:19 -0600

    update some css containers, tempo timesignature

--- metronome/src/components/tempoDropdown/tempoDropdown.components.jsx
+++ metronome/src/components/tempoDropdown/tempoDropdown.components.jsx
@@ -8,12 +8,13 @@ export default function TempoDropdown ({ onTempoSelect }) {
             <input
                 type='number'
                 defaultValue='60'
+                max="280"
+                min="35"
                 onInput={ (e) => {
                   e.preventDefault()
                   dropDownTempo = e.target.value
                   onTempoSelect(dropDownTempo)
                 }}
-                // onInput={ onTempoSelect }
             ></input>
         </>
   )
--- metronome/src/components/timeSignatureDropdown/timeSignatureDropdown.component.jsx
+++ metronome/src/components/timeSignatureDropdown/timeSignatureDropdown.component.jsx
@@ -5,20 +5,17 @@ export default function TimeSignatureDropdown ({ onBPMSelect }) {
   return (
     <>
       <label>Beats Per Measure</label>
-      <select onInput={ (e) => {
-        e.preventDefault()
-        selectMeasure = e.target.value
-        onBPMSelect(selectMeasure)
-      }}>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-        <option value="8">8</option>
-        <option value="9">9</option>
-      </select>
+      <input
+        type="number"
+        defaultValue="4"
+        max="12"
+        min="1"
+        onInput={(e) => {
+          e.preventDefault()
+          selectMeasure = e.target.value
+          onBPMSelect(selectMeasure)
+        }}
+      ></input>
     </>
   )
 }
